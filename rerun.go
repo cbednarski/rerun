406a852692a3ebe9b616ea87759ee3f8c97ef417
Chris Bednarski <banzaimonkey@gmail.com>
2015-06-08 18:56:16 -0700

    Implementation works now

--- rerun.go
+++ rerun.go
@@ -11,16 +11,30 @@ import (
 	"strings"
 )
 
+func fileExists(path string) bool {
+	if _, err := os.Stat(path); err == nil {
+		return true
+	}
+	return false
+}
+
 func main() {
 	if len(os.Args) <= 1 {
 		fmt.Println("Usage: rerun [command]")
 		os.Exit(1)
 	}
 
-	seen := map[string]int{}
+	total := 0
+	errors := 0
+	passes := 0
 
-	cmd := exec.Command(strings.Join(os.Args[1:], " "))
+	command := os.Args[1]
+	args := strings.Join(os.Args[2:], " ")
+	terminal := command + " " + args
+
 	for {
+		total++
+		cmd := exec.Command(command, os.Args[2:]...)
 		output, runtimeerr := cmd.CombinedOutput()
 
 		// This is a byte array; we need to convert it to a byte slice
@@ -28,17 +42,18 @@ func main() {
 		outputHash := hex.EncodeToString(hashbin[:])
 		filename := fmt.Sprintf("rerun-failure-%s.log", outputHash)
 
-		if runtimeerr != nil {
-			value, ok := seen[outputHash]
-			if ok {
-				seen[outputHash] = value + 1
-			} else {
-
-			}
-			seen[outputHash]++
-			ioerr := ioutil.WriteFile(filename, output, 0600)
-			if ioerr != nil {
-				log.Printf("Unable to write error log: %s", ioerr.Error())
+		if runtimeerr == nil {
+			passes++
+			log.Printf("%s SUCCEEDED (total: %d, passing: %d, errors: %d)", terminal, total, passes, errors)
+		} else {
+			errors++
+			// log.Printf(runtimeerr.Error())
+			log.Printf("%s FAILED (total: %d, passing: %d, errors: %d)", terminal, total, passes, errors)
+			if !fileExists(filename) {
+				ioerr := ioutil.WriteFile(filename, output, 0600)
+				if ioerr != nil {
+					log.Printf("Unable to write error log: %s", ioerr.Error())
+				}
 			}
 		}
 	}
